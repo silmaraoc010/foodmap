020fe2f8491061196adda9bb51844c8850145cd2
silmaraoc010 <silmara_a7x@hotmail.com>
2018-08-05 00:38:58 -0300

    Try again

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # foodmap
+
+1. F O O D M A P
+Crie um web-app que por meio de um input seja possível filtrar os restaurantes que estejam próximos a você. (Você deve decidir a estrutura que terão seus dados. Você pode criar uma lista de restaurantes com um *array ou um objeto, de forma individual. O projeto é totalmente livre)*.
+
+FLUXO DA APLICAÇÃO.
+Tela splash com duração de 2 a 5 segundos que redireciona para a página principal.
+
+Na página principal são apresentados todos os restaurantes "perto de você" junto com o input para filtrar sua seleção (os critérios de filtro são decisão sua; pode ser por tipo de comida, preço, ordem alfabética, etc.).
+
+
+Uma vez feita a seleção do filtro devem ser exibidos unicamente os restaurantes que atendam à condição.
+
+Ao selecionar algum dos restaurantes, será exibida a informação dele por meio de um modal.
+
+Uma vez fechado o modal, deve retornar à tela principal.
+
+<img src="tela 1.png">
--- js/app.js
+++ js/app.js
@@ -1,1 +1,111 @@
 // Add your JavaScript
+
+var map, infoWindow;
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      latitud:-23.2794319,
+      longitud: -46.7448066
+    },
+    zoom: 10
+  });
+  var infoWindow = new google.maps.InfoWindow({ map: map });
+
+
+  for (var i = 0; i < restaurantes.length; i++) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(restaurantes[i].latitude, restaurantes[i].longitude),
+      title: restaurantes[i].name,
+      map: map,
+      icon: 'assets/icon.png',
+
+    });
+
+    var infowindow = new google.maps.InfoWindow(), marker;
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(marker.title);
+        infowindow.open(map, marker);
+      }
+    })(marker))
+  }
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Olá, você está aqui!.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+   'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+//Splash screen
+$(document).ready(function () {
+  $('.screen-splash').delay('4000').fadeOut('slow');
+  $('.screen-main').delay('4000').fadeIn('slow'); 
+
+// Chamando os restaurantes
+     function insert(restaurantes) {
+    restaurantes.forEach((restaurante, index) => {
+      $('.result-rest').append('<div id="' + index + '" class="" data-toggle="modal" data-target="#modal' + index + '">  <div class="item"><img src="' + restaurante.image + '"> <h5 class="nameRestaurant" style="align-items: center">' + restaurante.name + '</h5></div></div>');
+      $('#' + index).click(function() {
+        $('.screen-main').append('<div class="modal" id="modal' + index + '" tabindex="-1" role="dialog"> <div class="modal-dialog" role="document"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title">'+ restaurante.name +'</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div class="modal-body"><img src="' + restaurante.image + '" width="150px" height="150px"><p>' + restaurante.description + '</p> <strong>' + restaurante.type +'</strong> </div> <div class="modal-footer"> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> </div> </div> </div> </div>');
+      });
+    });
+  }
+  insert(restaurantes);
+   
+  $('.type').each(function () {
+    $(this).on('click', function () {
+      searchByType($(this).val());
+    });
+  })
+
+  function searchByType(el) {
+    $('h5').each(function () {
+      if ($(this).text() !== el) {
+        $(this).parent().fadeOut('slow');
+      } if ($(this).text() === el) {
+        $(this).parent().fadeIn('slow');
+      }
+    })
+  }
+
+  $('.search').on('click', function () {
+    $('.nameRestaurant').each(search);
+  })
+  function search() {
+    if ($(this).text().toLowerCase() !== $('.input').val().toLowerCase()){
+      $(this).parent().fadeOut('slow');
+    } if ($(this).text() === $('.input').val()) {
+      $(this).parent().fadeIn('slow');
+    }
+  }
+  $('.input').on('input', function () {
+    if ($(this).val() === "") {
+      $('h5').each(function () {
+        $(this).parent().fadeIn('slow');
+      });
+    }
+  })
+})
+
